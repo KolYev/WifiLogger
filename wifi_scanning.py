ecad8ce669341d8275f7f742fcf9337f9b151089
KolYev <evgeniy.kolesenko@gmail.com>
2026-06-09 15:29:13 +0300

    wifi_scanning.py: узнаём внешний IP адрес и местоположение

--- wifi_scanning.py
+++ wifi_scanning.py
@@ -1,4 +1,34 @@
 from scapy.all import *
+import requests
 
+# локальный IP адрес
 router_ip = conf.route.route("0.0.0.0")[2]
 print(f"IP-адрес роутера: {router_ip}")
+
+# внешний IP адрес
+def get_ip_location():
+    try:
+        url = "http://ip-api.com/json/"
+        response = requests.get(url)
+        data = response.json()
+
+        if data.get('status') == 'success':
+            ip = data.get('query')
+            country = data.get('country')
+            region = data.get('regionName')
+            city = data.get('city')
+            coordinate_lat = data.get('lat')
+            coordinate_lon = data.get('lon')
+            
+            print(f"Внешний IP: {ip}")
+            print(f"Страна: {country}")
+            print(f"Регион: {region}")
+            print(f"Город: {city}")
+            print(f"Координаты: {coordinate_lat}, {coordinate_lon}")
+        else:
+            print(f"Ошибка сервиса: {data.get('message', 'Неизвестный сбой')}")
+        
+    except Exception as e:
+        print(f"Ошибка подключения: {e}")
+
+get_ip_location()
