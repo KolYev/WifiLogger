aafd57184498f4aa596d4f6092e2ec41fc0cbf3a
KolYev <evgeniy.kolesenko@gmail.com>
2026-06-09 14:41:24 +0300

    map.py: отображение карты мира на локальном веб-сервере

--- map.py
+++ map.py
@@ -1,5 +1,16 @@
 import folium
+import http.server
+import socketserver
+import webbrowser
+import os
 
 world_map = folium.Map()
-
 world_map.save("world_map.html")
+
+PORT = 8000
+os.chdir(os.path.dirname(os.path.abspath('world_map.html')))
+
+with socketserver.TCPServer(("", PORT), http.server.SimpleHTTPRequestHandler) as httpd:
+    webbrowser.open(f'http://localhost:{PORT}/world_map.html')
+    httpd.serve_forever()
+    
